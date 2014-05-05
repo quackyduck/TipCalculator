a86f2fdd7d9f07ae2a8cf18d82b1802f56f20a40
Nicolas Melo <nicolas@path.com>
2014-05-04 17:20:48 -0700

    Initial Commit

---
+++ TipCalculator/NMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NMAppDelegate.h
+//  TipCalculator
+//
+//  Created by Nicolas Melo on 5/4/14.
+//  Copyright (c) 2014 melo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Nicolas Melo on 5/4/14.
+//  Copyright (c) 2014 melo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NMAppDelegate class]));
+    }
+}
